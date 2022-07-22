095dedf04419745c22f76b526ebf01abac27ede1
jmichaeltoohey <57685360+jmichaeltoohey@users.noreply.github.com>
2022-07-22 17:55:48 -0400

    Delete temps_t_test.R

--- temps_t_test.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-
-temp.liz_F <- na.omit(diagnosed.forest$TEMP.LIZ)
-temp.sub_F <- na.omit(diagnosed.forest$TEMP.SUB)
-temp.liz_U <- na.omit(diagnosed.urban$TEMP.LIZ)
-temp.sub_U <- na.omit(diagnosed.urban$TEMP.SUB)
-
-t.test(temp.liz_F, temp.liz_U)
-t.test(temp.sub_F, temp.sub_U)
